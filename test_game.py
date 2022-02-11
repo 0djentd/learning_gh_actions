0b11dbb600c69b812824a5573211a8e680beedb1
0djent <0djent@github.com>
2022-02-11 19:37:11 +0300

    modified:   test_game.py

--- test_game.py
+++ test_game.py
@@ -1,13 +1,31 @@
 """Unittests"""
 
 import unittest
+import copy
+
 import game
 
 
 class GameTests(unittest.TestCase):
     """Unittests"""
 
-    def test_player(self):
+    def setUp(self):
         """Test player name."""
-        player = game.Player('a')
-        self.assertEqual(player.name, 'a')
+        self.names = ['player 1', 'player 2']
+
+    def tearDown(self):
+        """Test player name."""
+        del self.names
+
+    def test_names(self):
+        """Test player name."""
+        for x in self.names:
+            if not isinstance(x, str):
+                raise TypeError
+
+    def test_player_name(self):
+        """Test player name."""
+        for x in self.names:
+            player = game.Player(copy.deepcopy(x))
+            with self.subTest():
+                self.assertEqual(player.name, x)
