05fdad6d491c823d39bb55f775b475dbd43f8460
0djent <0djent@github.com>
2022-02-11 17:49:08 +0300

    Added docstrings.

--- game.py
+++ game.py
@@ -4,25 +4,33 @@ import logging
 import dataclasses
 
 
+"""Simple game.
+"""
+
+
 @dataclasses.dataclass
 class Player():
+    """Object representing player."""
     name: str
 
 
 @dataclasses.dataclass
 class GameState():
+    """Object representing game state."""
     players: list[Player]
     score: int = 0
     max_score: int = 3
     last: Player | None = None
 
     def check(self) -> bool:
+        """Check if game should be finished."""
         if self.score >= self.max_score:
             return True
         return False
 
 
 def main() -> int:
+    """Main function."""
     players = [Player('a'), Player('b')]
     game = GameState(players)
     print(game)
