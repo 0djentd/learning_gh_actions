706e0cea7723bbdce40661aa530ee9ee8962b31b
0djent <0djent@github.com>
2022-02-11 18:18:58 +0300

    modified:   test_game.py

--- test_game.py
+++ test_game.py
@@ -1,8 +1,13 @@
 import unittest
 import game
 
+"""Unittests"""
+
 
 class GameTests(unittest.TestCase):
+    """Unittests"""
+
     def test_player(self):
+        """Test player name."""
         player = game.Player('a')
         self.assertEqual(player.name, 'a')
